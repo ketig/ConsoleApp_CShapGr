d5194fbd51a8f54ef842fc0f58f354d633a6d140
ketig <41895005+ketig@users.noreply.github.com>
2024-12-07 19:46:40 +0400

    Exception

--- ConsoleApp_CShapGr/Program.cs
+++ ConsoleApp_CShapGr/Program.cs
@@ -2,11 +2,26 @@
 
 string[] names = new string[4];
 
+try
+{
+    names[0] = "giorgi";
+    names[1] = "rati";
+    names[2] = "nika1";
+    names[3] = "nika2";
+} // block
+catch (IndexOutOfRangeException ex)
+{
+    Console.WriteLine("Index out of range!");
+}
+catch (Exception ex)
+{
+    Console.WriteLine("Oops, something went wrong!");
+}
+finally
+{
+    Console.WriteLine("Values inserted in array!");
+}
 
-names[0] = "giorgi";
-names[1] = "rati";
-names[2] = "nika1";
-names[3] = "nika2";
 
 for (int i = 0; i < names.Length; i++)
 {
