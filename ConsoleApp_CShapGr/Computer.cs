e430d39b2e0e45fff04d98292266edd98e1b7763
ketig <41895005+ketig@users.noreply.github.com>
2025-01-15 23:16:04 +0400

    Prop

--- ConsoleApp_CShapGr/Computer.cs
+++ ConsoleApp_CShapGr/Computer.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection.Metadata.Ecma335;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,12 +9,20 @@ namespace ConsoleApp_CShapGr
 {
     class Computer
     {
-        string _color;
-        string _gpu;
-        string _cpu;
-        string _ram;
-        string _brand;
-        string _model;
-        uint _quantity;
+        private string _color;
+        private string _gpu;
+        private string _cpu;
+        private string _ram;
+        private string _brand;
+        private string _model;
+        private uint _quantity;
+
+        public string Color { get => _color; set => _color = value; }
+        public string GPU { get => _gpu; set => _gpu = value; }
+        public string CPU { get => _cpu; set => _cpu = value; }
+        public string RAM { get => _ram; set => _ram = value; }
+        public string Brand { get => _brand; set => _brand = value; }
+        public string Model { get => _model; set => _model = value; }
+        public uint Quantity { get => _quantity; set => _quantity = value; }
     }
 }
